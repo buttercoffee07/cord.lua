5f97d7a2b0d7c88c7b453a0297763f01ff4b04f0
Ellie <buttercoffee07@gmail.com>
2026-03-05 19:16:56 +0000

    feat(client): compose rest client, gateway, and event emitter

--- src/client.lua
+++ src/client.lua
@@ -1,4 +1,7 @@
 local Class = require("src.core.class")
+local EventEmitter = require("src.core.event_emitter")
+local RestClient = require("src.http.rest_client")
+local Gateway = require("src.gateway.gateway")
 
 local Client = Class.extend()
 
@@ -7,6 +10,31 @@ function Client:init(opts)
 
 	self.token = opts.token
 	self.intents = opts.intents or 0
+
+	self.events = opts.events or EventEmitter.new()
+
+	self.rest = opts.rest or RestClient.new({
+		token = self.token,
+		rateLimiter = opts.rateLimiter,
+		baseUrl = opts.baseUrl,
+		requestFn = opts.requestFn,
+		json = opts.restJson or opts.json,
+	})
+
+	self.gateway = opts.gateway or Gateway.new({
+		token = self.token,
+		intents = self.intents,
+		gatewayUrl = opts.gatewayUrl,
+		wsFactory = opts.wsFactory,
+		json = opts.gatewayJson or opts.json,
+		spawn = opts.spawn,
+		sleep = opts.sleep,
+		identifyProperties = opts.identifyProperties,
+		autoReconnect = opts.autoReconnect,
+		reconnectBaseDelay = opts.reconnectBaseDelay,
+		reconnectMaxDelay = opts.reconnectMaxDelay,
+		random = opts.random,
+	})
 end
 
 return Client
